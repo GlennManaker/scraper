65c6b48edfce96d710232f069a1c7c1b3d4367fb
GlennManaker <37516071+GlennManaker@users.noreply.github.com>
2021-02-21 17:06:57 +0300

    fixes

--- main.py
+++ main.py
@@ -54,4 +54,4 @@ if __name__ == "__main__":
     # _s = threading.Thread(target = scraper)
     # _s.start()
 
-    server.run(host="127.0.0.1", port=int(os.environ.get("PORT", 5000)))
+    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))

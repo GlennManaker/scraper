1b763c65474a3899e4f13a0b533acf079bc04442
GlennManaker <37516071+GlennManaker@users.noreply.github.com>
2021-02-25 23:33:45 +0300

    fixes

--- api/stats.py
+++ api/stats.py
@@ -6,27 +6,27 @@ from main import _mM
 @stats_endpoint.route("/api/get_stats/<int:id>", methods=['GET'])
 def get_stats(id):
     _r_q = request.args.get('filter')
-    _info = _mM.find({'url': os.getenv('URL_GET_GAME').format(id)})
+    _info = _mM.find({'I': id})
     data = []
     for _t in _info:
         if (_r_q is not None):
             values = []
-            for _e in _t['keys']:
+            for _e in _t['S']:
                 print(_r_q)
-                print(_e['Key'])
                 if (_r_q in _e['Key']):
                     print(_e)
                     values.append(_e)
-            data.append({'time': int(_t['_id'].generation_time.timestamp()), 'url': _t['url'], 'value': values})
+            data.append({'time': _t['TS'], 'I': _t['I'], 'value': values})
         else:
-            data.append({'time': int(_t['_id'].generation_time.timestamp()), 'url': _t['url'], 'values': _t['keys']})
+            data.append({'time': _t['TS'], 'I': _t['I'], 'values': _t['S']})
     return jsonify(data)
 
 
 @stats_endpoint.route("/api/available_ids", methods=['GET'])
 def get_available_id():
     data = []
-    _info = _mM.distinct("url")
+    _info = _mM.distinct("I")
     for _t in _info:
-        data.append(_t[_t.find('=') + 1:_t.find('&')])
+        print(_t)
+        data.append(_t)
     return jsonify({'ids': data})
